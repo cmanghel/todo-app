6ea2c6f31c12badd49f5c0f4c5d9beb7bb893c56
cmanghel <cata.anghel@gmail.com>
2020-08-02 19:55:18 +0300

    Updates

---
+++ precache-manifest.5b76f7175f601e992686bd56dd76d254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f23ae8a2b61d39de183128a765d6795",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "311248913af37d50cd74",
+    "url": "/todo-app/static/css/main.e05bd2ff.chunk.css"
+  },
+  {
+    "revision": "9fb969920926dd47c61e",
+    "url": "/todo-app/static/js/2.19cc511d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.19cc511d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311248913af37d50cd74",
+    "url": "/todo-app/static/js/main.7b719c3d.chunk.js"
+  },
+  {
+    "revision": "017594b70fcddfe941fd",
+    "url": "/todo-app/static/js/runtime-main.94e975c4.js"
+  }
+]);
